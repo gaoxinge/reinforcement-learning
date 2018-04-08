d596a9d39ad3a303d77f01d0e0d1a2fe915e9a73
高辛格 <xg.gao@tianrang-inc.com>
2018-04-08 21:02:30 +0800

    dqn

---
+++ dqn/test1/agent.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+import random
+
+
+def _all_equal(actions):
+    for action in actions:
+        for action_ in actions:
+            if actions[action] != actions[action_]:
+                return False
+    return True
+
+
+class QLearnAgent(object):
+
+    def __init__(self, act_n, linear_func, gamma=0.9, epsilon=0.1, learning_rate=0.01):
+        self.act_n = act_n
+        self.linear_func = linear_func
+        self.gamma = gamma
+        self.epsilon = epsilon
+        self.learning_rate = learning_rate
+
+    def choose(self, state):
+        actions = {a: self.linear_func(state, a) for a in range(self.act_n)}
+        if random.random() < self.epsilon or _all_equal(actions):
+            return random.choice(range(self.act_n))
+        else:
+            m = max(actions.values())
+            for k, v in actions.items():
+                if v == m:
+                    return k
+
+    def learn(self, state, action, reward, state_, done):
+        predict = self.linear_func(state, action)
+        actions = {a: self.linea_func(state, a) for a in range(self.act_n)}
+        target = reward if done else reward + self.gamma * max(actions.values())
+        error = target - predict
+        self.linear_func.update(self.learning_rate * error, state, action)
