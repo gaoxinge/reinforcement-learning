2f908d810f7e5dd2796f7b5e4bf467cf1864ffc8
高辛格 <xg.gao@tianrang-inc.com>
2018-04-27 12:39:25 +0800

    dqn

---
+++ dqn/test5/demo.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+import matplotlib.pyplot as plt
+import gym
+from agent import QLearnAgent
+
+
+def gym_demo(env, agent, episodes):
+    r, rs = 0, []
+    for episode in range(episodes):
+        total_reward = 0
+        raw_state = env.reset()
+        state = [raw_state]
+        while True:
+            env.render()
+            action = agent.choose(state)
+            raw_state_, reward, done, prob = env.step(action)
+            state_ = [raw_state_]
+            agent.store(state, action, reward, state_, done)
+            if len(agent.memory) > agent.batch_size:
+                agent.learn()
+            state = state_
+            total_reward += reward
+            if done:
+                env.render()
+                print('episode: {episode}, total reward: {total_reward}'.format(
+                    episode=episode,
+                    total_reward=total_reward
+                ))
+                r += total_reward
+                rs.append(r / (episode + 1))
+                break
+    return rs
+    
+
+def test_gym():
+    env = gym.make('MountainCar-v0').env
+    agent = QLearnAgent(state_n=2, act_n=3)
+    rs = gym_demo(env, agent, 1000)
+    plt.plot(range(1000), rs), plt.grid(), plt.show()
+
+
+test_gym()
