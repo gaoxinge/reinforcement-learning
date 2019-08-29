a779952e6862edcad0517d714781ecbd03eb3e91
gaoxinge <gaoxx5@gmail.com>
2019-08-29 03:27:51 +0000

    policy gradient

--- policy gradient/test2/demo.py
+++ policy gradient/test2/demo.py
@@ -9,15 +9,15 @@ def gym_demo(env, agent, episodes):
     for episode in range(episodes):
         total_reward = 0
         state = env.reset()
-        while True:
-            env.render()
+        for _ in range(500):
+            # env.render()
             action = agent.choose(state)
             state_, reward, done, prob = env.step(action)
             agent.store(state, action, reward)
             state = state_
             total_reward += reward
             if done:
-                env.render()
+                # env.render()
                 print('episode: {episode}, total reward: {total_reward}'.format(
                     episode=episode,
                     total_reward=total_reward
@@ -26,11 +26,20 @@ def gym_demo(env, agent, episodes):
                 rs.append(r / (episode+1))
                 agent.learn()
                 break
+        else:
+            # env.render()
+            print('episode: {episode}, total reward: {total_reward}'.format(
+                episode=episode,
+                total_reward=total_reward
+            ))
+            r += total_reward
+            rs.append(r / (episode+1))
+            agent.learn()
     return rs
 
     
 def test_gym():
-    env = gym.make('CartPole-v1')
+    env = gym.make('CartPole-v1').env
     agent = MonteCarloPolicyGradientAgent(4, 2)
     rs = gym_demo(env, agent, 1000)
     plt.plot(range(1000), rs), plt.grid(), plt.show()
